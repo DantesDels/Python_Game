8a3440d71df666c26ce10b20d11a2ca5598e5929
DantesDels <delver.sebastien@gmail.com>
2025-10-17 18:49:43 +0200

    feature: separate difficulty_manager from game.py

--- island-survival-game/src/__main__.py
+++ island-survival-game/src/__main__.py
@@ -2,9 +2,10 @@ from player import Player
 from game import Game
 
 def main():
-    player = Player("Survivant")    
-    game = Game(player) 
-    game.start_game()   
+    player = Player("Survivant")
+    game = Game(player)
+    game.start_game()
+    
 
 if __name__ == "__main__":
     main()  
--- island-survival-game/src/game.py
+++ island-survival-game/src/game.py
@@ -1,6 +1,4 @@
-import json
 from constants import *
-from player import Player
 from utils import format_gauge
 
 class Game:
@@ -61,33 +59,3 @@ class Game:
             self.player.reset()
         self.day = 1
         self.is_game_over = False
-
-
-    def difficulty_manager(difficulty, days_survived):
-       difficulty = open('difficulty_multipliers.json', 'r')
-       difficulty_data = json.load(difficulty)
-       difficulty.close()
-
-       days_survived = Player.self.days_survived
-
-       if difficulty_data.get(difficulty):
-           settings = difficulty_data[difficulty]
-           Player.self.hunger += settings.get("hunger")
-           Player.self.thirst += settings.get("thirst")
-           Player.self.energy += settings.get("energy")
-           
-       else:
-           print("Unknown difficulty level. Default settings applied.")
-           settings = difficulty_data["Normal"]
-           Player.self.hunger += settings.get("hunger")
-           Player.self.thirst += settings.get("thirst")
-           Player.self.energy += settings.get("energy")
-
-       for days_survived in range(difficulty["days_left"]):
-           days_survived += 1
-           if days_survived == difficulty["days_left"]:
-               break
-           else:
-               daily_mult_data += settings.get(difficulty["daily_growth"])
-           pass
-
