bbb0997dec6d5c47bd1e2cdae66648c3ae73fcce
DantesDels <delver.sebastien@gmail.com>
2025-10-19 20:02:54 +0200

    refactor: improve load_json_filename function for better file handling in difficulty_manager

--- island-survival-game/src/difficulty_manager.py
+++ island-survival-game/src/difficulty_manager.py
@@ -1,9 +1,15 @@
 from player import Player
 import json
+import os
 
-def difficulty_manager(difficulty_level):
-    difficulty = open('../res/difficulty_level.json', 'r', encoding='utf-8')
-    difficulty_data = json.load(difficulty)
-    difficulty.close()
-    return difficulty_data.get(difficulty_level, difficulty_data["Baby"])
- 
+def load_json_filename(filename):
+    base = os.path.dirname(__file__)
+    path = os.path.join(base, '..', 'res', filename)
+    with open(path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def difficulty_manager(difficulty_level, difficulty_player):
+    level_difficulty_data = load_json_filename('difficulty_level.json')
+    player_difficulty_data = load_json_filename('difficulty_player.json')
+    return player_difficulty_data.get(difficulty_player, player_difficulty_data.get("Baby"))
+
